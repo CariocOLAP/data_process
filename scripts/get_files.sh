81b04789f8249b4d3f738f7d82ffa643f0622c33
Gabriel Sá Rodrigues <gabriel.desar@gmail.com>
2017-02-12 11:10:47 -0200

    fixing update script

--- scripts/get_files.sh
+++ scripts/get_files.sh
@@ -1,2 +1,2 @@
 #usage get_files {SERVER_DOMAIN} {ZIP_FOLDERS}
-rsync -tP $1:$2/* ../bus_data/
+rsync -tP $1:$2/* ./data/datario/bus/
--- update.sh
+++ update.sh
@@ -15,13 +15,13 @@ USERNAME=$4
 #Downloading data from database server
 scripts/get_files.sh $SERVER_DATA $GPS_FOLDER
 
-#Getting Data Into Local Database
+echo "Getting Data Into Local Database"
 sh kettle/kitchen.sh -file="../ktrs/datario/1_endpoint_crossing/1 - Starter.kjb" -param:DATABASE_NAME=$DATABASE_NAME -param:PASSWORD=$PASSWORD -param:PORT_NUMBER=$PORT_NUMBER -param:USERNAME=$USERNAME -param:HOSTNAME=$HOSTNAME
 
-#Generating Real Trips from GPS Data
+echo "Generating Real Trips from GPS Data"
 
 sh kettle/pan.sh -file="../ktrs/datario/2_trip_generation/gen_trip.ktr" -param:DATABASE_NAME=$DATABASE_NAME -param:PASSWORD=$PASSWORD -param:PORT_NUMBER=$PORT_NUMBER -param:USERNAME=$USERNAME -param:HOSTNAME=$HOSTNAME
 
-#Transform Data to Dimensions and Fact
+echo "Transform Data to Dimensions and Fact"
 
 sh kettle/kitchen.sh -file="../ktrs/datario/3_dw_development/export_to_dw.kjb" -param:DATABASE_NAME=$DATABASE_NAME -param:PASSWORD=$PASSWORD -param:PORT_NUMBER=$PORT_NUMBER -param:USERNAME=$USERNAME -param:HOSTNAME=$HOSTNAME
